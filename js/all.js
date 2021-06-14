de25becdb6a2642b0d9f748a9357aa49e75f8f76
Vicky.Lin <vicky.lin@tech-trans.com>
2021-06-14 12:50:00 +0800

    feat: 版型大致完成

---
+++ js/all.js
@@ -0,0 +1,5 @@
+// 初始化bootstrap tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
